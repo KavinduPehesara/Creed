baf0e5f16486f28c5ec0ca9c34020b91a9fb589c
Kavindu Pehesara Gunawardene <kavindupehesara12090@gmail.com>
2021-06-23 22:45:37 +0530

    LL

--- Pramod/PointForm/PointForm/Form1.Designer.cs
+++ Pramod/PointForm/PointForm/Form1.Designer.cs
@@ -29,36 +29,121 @@ namespace PointForm
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.IDTX = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.NameTX = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.HouseTX = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // textBox1
+            // IDTX
             // 
-            this.textBox1.Location = new System.Drawing.Point(238, 79);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(142, 23);
-            this.textBox1.TabIndex = 0;
+            this.IDTX.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.IDTX.Location = new System.Drawing.Point(175, 168);
+            this.IDTX.Name = "IDTX";
+            this.IDTX.Size = new System.Drawing.Size(105, 26);
+            this.IDTX.TabIndex = 0;
+            this.IDTX.Text = "5";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Sitka Small", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(85, 79);
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(55, 172);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(92, 23);
+            this.label1.Size = new System.Drawing.Size(85, 19);
             this.label1.TabIndex = 1;
             this.label1.Text = "StudentID";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(116)))), ((int)(((byte)(138)))), ((int)(((byte)(2)))));
+            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBox1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.IntegralHeight = false;
+            this.comboBox1.Location = new System.Drawing.Point(55, 25);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(483, 38);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // NameTX
+            // 
+            this.NameTX.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.NameTX.Location = new System.Drawing.Point(303, 168);
+            this.NameTX.Name = "NameTX";
+            this.NameTX.ReadOnly = true;
+            this.NameTX.Size = new System.Drawing.Size(235, 26);
+            this.NameTX.TabIndex = 3;
+            this.NameTX.Text = "Name";
+            this.NameTX.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(238, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "First Place";
+            // 
+            // HouseTX
+            // 
+            this.HouseTX.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.HouseTX.Location = new System.Drawing.Point(433, 213);
+            this.HouseTX.Name = "HouseTX";
+            this.HouseTX.ReadOnly = true;
+            this.HouseTX.Size = new System.Drawing.Size(105, 26);
+            this.HouseTX.TabIndex = 6;
+            this.HouseTX.Text = "House";
+            this.HouseTX.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(422, 379);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 33);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Next";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(422, 326);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(116, 33);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Check";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(599, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.HouseTX);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.NameTX);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.IDTX);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -66,8 +151,14 @@ namespace PointForm
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox IDTX;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox NameTX;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox HouseTX;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Pramod/PointForm/PointForm/Form1.cs
+++ Pramod/PointForm/PointForm/Form1.cs
@@ -7,15 +7,80 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+
 
 namespace PointForm
 {
     public partial class Form1 : Form
     {
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Kavin\Desktop\Creed\Pramod\Creed.mdf;Integrated Security=True;Connect Timeout=30");
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int STDID = int.Parse(IDTX.Text);
+            string qur = "SELECT * FROM StudentDB WHERE StudentIndex = " + STDID + "";
+            SqlCommand cmd = new SqlCommand(qur, con);
+
+            try
+            {
+                con.Open();
+                SqlDataReader rd = cmd.ExecuteReader();
+                while (rd.Read())
+                {
+                    NameTX.Text = rd[1].ToString();
+                    HouseTX.Text = rd[3].ToString();
+                }
+                MessageBox.Show("Data Find Successfully");
+
+            }
+            catch (SqlException se)
+            {
+                MessageBox.Show(se.ToString());
+            }
+            finally
+            {
+                con.Close();
+                //display_data();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int SPID = 1;
+            int STID = int.Parse(IDTX.Text);
+            int MARK = int.Parse(MarkTX.Text);
+            string House = HouseTX.Text;
+
+            string qur = "INSERT INTO PointDB VALUES (" + SPID + ",' " + STID + " ' , ' " + MARK + " ' , ' " + House + " ' )";
+
+            SqlCommand cmd = new SqlCommand(qur, con);
+
+            try
+            {
+                con.Open();
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Data Insert Successfully");
+            }
+            catch (SqlException se)
+            {
+                MessageBox.Show(se.ToString());
+            }
+            finally
+            {
+                con.Close();
+                //display_data();
+
+            }
+        }
     }
 }
